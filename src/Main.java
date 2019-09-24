789558882e6565306ed3aad7e13ffa033f953703
8pade8 <8pade8@gmail.com>
2019-09-24 09:09:25 +0300

    Добавлен вывод результата проверки в человеческом виде

--- src/Main.java
+++ src/Main.java
@@ -15,13 +15,21 @@ import java.util.concurrent.SynchronousQueue;
 public class Main {
 
     public static void main(String[] args) {
-        String inn = "";
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            inn = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
+        String inn = "";
+
+        while (true){
+            System.out.println("Введите ИНН контрагента");
+            try {
+                inn = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (!inn.equals("")) {
+                break;
+            }
         }
+
         FNSNDSCAWS2 fnsndscaws2 = new FNSNDSCAWS2();
         FNSNDSCAWS2Port fnsndscaws2Port = fnsndscaws2.getFNSNDSCAWS2Port();
 
@@ -30,6 +38,7 @@ public class Main {
 
         NdsRequest2.NP np = new NdsRequest2.NP();
         np.setINN(inn);
+
         SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
         np.setDT(format.format(new Date()));
 
@@ -37,7 +46,51 @@ public class Main {
 
         NdsResponse2 response = fnsndscaws2Port.ndsRequest2(ndsRequest);
 
-        System.out.println(response.getNP().get(0).getState());
+        String stateString = response.getNP().get(0).getState();
+        int state = Integer.parseInt(stateString);
+        System.out.println(state);
+        switch (state) {
+            case 0:
+                System.out.println("Налогоплательщик зарегистрирован в ЕГРН и имел статус действующего в указанную дату");
+                break;
+            case 1:
+                System.out.println("Налогоплательщик зарегистрирован в ЕГРН, но не имел статус действующего в указанную дату");
+                break;
+            case 2:
+                System.out.println("Налогоплательщик зарегистрирован в ЕГРН");
+                break;
+            case 3:
+                System.out.println("Налогоплательщик с указанным ИНН зарегистрирован в ЕГРН, КПП не соответствует ИНН или не указан");
+                break;
+            case 4:
+                System.out.println("Налогоплательщик с указанным ИНН не зарегистрирован в ЕГРН");
+                break;
+            case 5:
+                System.out.println("Некорректный ИНН");
+                break;
+            case 6:
+                System.out.println("Недопустимое количество символов ИНН");
+                break;
+            case 7:
+                System.out.println("Недопустимое количество символов КПП");
+                break;
+            case 8:
+                System.out.println("Недопустимые символы в ИНН");
+                break;
+            case 9:
+                System.out.println("Недопустимые символы в ИНН");
+                break;
+            case 10:
+                System.out.println("КПП не должен использоваться при проверке ИП");
+                break;
+            case 11:
+                System.out.println("некорректный формат даты");
+                break;
+            case 12:
+                System.out.println("некорректная дата (ранее 01.01.1991 или позднее текущей даты)");
+                break;
+        }
+
         try {
             reader.readLine();
         } catch (IOException e) {
